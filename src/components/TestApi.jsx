b2417f0ce50bedd1c69f3e04a542184ba4dac3e1
jw6133 <jw6133@naver.com>
2024-03-28 00:48:20 +0900

    api troubleshooting

--- src/components/TestApi.jsx
+++ src/components/TestApi.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import axios from 'axios';
 
 const TestApi = () => {
     const key="rkxhfflreogkrrywkflsrhql2024";
@@ -6,10 +7,32 @@ const TestApi = () => {
         initApi()
     },[]);
 
+    // async function initApi(){
+    //     try{
+    //         const result = await axios.get('https://capstone.hyunn.site/api/user/images'
+    //         // ,{
+    //         //     headers:{
+    //         //         Authorization:"rkxhfflreogkrrywkflsrhql2024",
+    //         //         "phone": "01012345678",
+    //         //         "email": "root@naver.com"
+    //         //     },
+    //         // }
+    //         );
+    //         console.log(result);
+    //     }
+    //     catch(error){
+    //         console.error(error);
+    //     }
+    // }
     async function initApi(){
         try{
-            // const result = await axios.get('https://capstone.hyunn.site/swagger-ui/index.html');
-            // console.log(result);
+            const result = await axios.post('/api/user/images',{
+                withCredentials:true,
+                headers:{ 'x-api-key':"testapieky1234"},
+                "phone": "01012345678",
+                "email": "root@naver.com",
+            });
+            console.log(result);
         }
         catch(error){
             console.error(error);
