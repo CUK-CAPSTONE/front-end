8102f1e3986c3b69f4d30133cb5d36d7dbdfaf77
jw6133 <jw6133@naver.com>
2024-04-03 00:35:39 +0900

    fix the init no wheel action

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -24,6 +24,7 @@ const Main = () => {
       setCurrentInputs({currentWindowHeight:window.innerHeight})
     };
 
+
     const setPage=()=>{
       for(var i=1;i<totalPageNumber;i++){
         if(window.scrollY<currentInputs.currentWindowHeight*i){
@@ -32,44 +33,53 @@ const Main = () => {
         }
       }
     };
-
     useEffect(()=>{
+      setPageSize();
+      setPage();
+    },[]);
+    
+    useEffect(()=>{
+      wheelAction();
       window.addEventListener('scroll',setPage);
       window.addEventListener('resize',setPageSize);
+      
       return ()=>{
         window.removeEventListener("scroll",setPage);
         window.removeEventListener("resize",setPageSize);
       };
     });
-  
-    window.addEventListener("wheel", (e) => {
-      // 마우스 휠을 내릴때
-      if (e.deltaY > 0) {
-        let p = 1;
-        while (p < totalPageNumber) {
-          if (currentInputs.currentPage === p) {
-            window.scrollTo({
-              top: currentInputs.currentWindowHeight * p,
-              behavior: "smooth",
-            });
+
+    function wheelAction(){
+      window.addEventListener("wheel", (e) => {
+        // 마우스 휠을 내릴때
+        if (e.deltaY > 0) {
+          let p = 1;
+          while (p < totalPageNumber) {
+            if (currentInputs.currentPage === p) {
+              window.scrollTo({
+                top: currentInputs.currentWindowHeight * p,
+                behavior: "smooth",
+              });
+            }
+            p++;
           }
-          p++;
         }
-      }
-      // 마우스 휠을 올릴때
-      if (e.deltaY < 0) {
-        let p = 1;
-        while (p < totalPageNumber) {
-          if (currentInputs.currentPage === p) {
-            window.scrollTo({
-              top: currentInputs.currentWindowHeight * (p - 1),
-              behavior: "smooth",
-            });
+        // 마우스 휠을 올릴때
+        if (e.deltaY < 0) {
+          let p = 1;
+          while (p < totalPageNumber) {
+            if (currentInputs.currentPage === p) {
+              window.scrollTo({
+                top: currentInputs.currentWindowHeight * (p - 1),
+                behavior: "smooth",
+              });
+            }
+            p++;
           }
-          p++;
         }
-      }
-    });
+      });
+    }
+    
 
     const navigate = useNavigate();
 
